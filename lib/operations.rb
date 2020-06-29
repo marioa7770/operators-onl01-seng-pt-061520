3ff89e7154f73f20c5a49b8f58bbc2d94e49ca9c
mario acevedo <jun_acevedo415@yahoo.com>
2020-06-29 01:06:15 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,10 @@
-def unsafe?(speed)
-
-end
-
-
-
-def not_safe?(speed)
-	
-end
-	
-
-
+def unsafe?(speed)	def unsafe?(speed)
+	if speed > 60 	
+		true	
+	elsif 	
+		speed < 40	
+		true	
+	else	
+		false	
+	end	
+end	end
